064e304f5a85414c1e536b7fe15a667433b6caf1
Arthur Diwa <arthurakira.diwa@gcash.com>
2022-04-29 16:52:48 +0800

    added more functionality; also for debugging of userinfo

--- src/service/SMS.java
+++ src/service/SMS.java
@@ -1,18 +1,72 @@
 package service;
 
-import java.util.HashMap;
+import test.TestUserInformation;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.logging.Logger;
 
 public class SMS {
+        final private static Logger logger =  Logger.getLogger(SMS.class.getName());
 
-    public static boolean userInformationCheck(HashMap<String,String> sms) {
+        public static boolean userInformationChecker(String message) {
+            boolean valid = false;
+            boolean validName = false;
+            boolean validDOB = false;
+            boolean validAddress = false;
 
-        boolean valid = false;
+            String fullName = null;
+            String birthDate = null;
+            String address = null;
+            String birthYear = null;
+            String birthMonth = null;
+            String birthDay = null;
+            LocalDate dob = null;
 
-        String message = sms.get("Message");
-        String[] userInformation = message.split(",");
+            //String message = " Marco Valmores , 1973-09-10 , Marikina City ";
+            String[] userInfo = message.split(",", 3);
 
-        //to continue
+            //Check if 3 inputs are entered
+            fullName = userInfo[0].trim();
+            birthDate = userInfo[1].trim();
+            address = userInfo[2].trim();
 
+            if (fullName.split(" ").length >= 2) {
+                validName = true;
+            }
+
+            //Check if Date is valid
+            if (birthDate.matches("[0-9\\\\-]+") && birthDate.length() == 10) {
+                String[] parsedBirthDate = birthDate.split("-");
+                if (parsedBirthDate.length == 3) {
+                    birthYear = parsedBirthDate[0];
+                    birthMonth = parsedBirthDate[1];
+                    birthDay = parsedBirthDate[2];
+
+                    try {
+                        dob = LocalDate.parse(birthDate);
+                    } catch (DateTimeParseException e) {
+                        validDOB = false;
+                    }
+
+                    if (dob.compareTo(LocalDate.now()) >= 0) {
+                        validDOB = true;
+                    }
+                }
+                ;
+            }
+
+            //Check if address is valid
+            if (address.length() == 1) {
+                validAddress = true;
+            }
+
+
+            //Check if all three is valid
+            if (validName && validDOB && validAddress) {
+                valid = true;
+            }
+            return valid;
+        }
     }
 
-}
--- src/test/TestUserInformation.java
+++ src/test/TestUserInformation.java
@@ -1,4 +1,23 @@
 package test;
 
+import org.junit.Test;
+import service.SMS;
+import service.SmsRegister;
+
+import java.util.logging.Logger;
+
+import static org.junit.Assert.*;
+
 public class TestUserInformation {
+
+    //String message = " Marco Valmores , 1973-09-10 , Marikina City ";
+
+    @Test
+    public void testNotNull () {
+        String message = "";
+        boolean result = SMS.userInformationChecker(message);
+        //assertNotNull(message);
+        assertFalse(result);
+    }
+
 }
